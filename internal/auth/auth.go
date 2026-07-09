f1081746225551dbb62810a3370469fdd8bcb079
dev-karani <edwin.k.kithinji@gmail.com>
2026-07-09 13:47:08 +0300

    add auth func to get apikey

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -26,6 +26,29 @@ func MakeRefreshToken() (string, error) {
 	}
 	return hex.EncodeToString(key), nil //convert it to hex string
 }
+
+func GetAPIKey(headers http.Header) (string,error) {
+	authorizationString := headers.Get("Authorization")
+
+	//solve if its empty
+	if authorizationString == "" {
+		return "", errors.New("missing Authorization header")
+	}
+    
+	//strip the apikey prefiv
+    _, after, found := strings.Cut(authorizationString, "ApiKey ")
+	//if apikey prefix not found
+    if !found {
+     	return "", errors.New("authorization header must start with \"ApiKey"\")
+	}
+    after = strings.TrimSpace(after)
+	if after == "" {
+		return "", errors.New("missing token")
+	}
+
+	return after, nil
+
+}
 func GetBearerToken(headers http.Header) (string, error) {
 	authString := headers.Get("Authorization")
 	//solve if its empty
